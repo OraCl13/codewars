58e37e52c9a7fe5e09835f6138fe4b61955779d6
OraCl13 <ostap.pelo@gotoinc.co>
2022-09-05 15:13:52 +0300

    diagonal to do

---
+++ diagonal_product/solution.rb
@@ -0,0 +1,22 @@
+MSSV = [[1, 4, 7, 6, 5],
+        [-3, 2, 8, 1, 3],
+        [6, 2, 9, 7, -4],
+        [1, -2, 4, -2, 6],
+        [3, 2, 2, -4, 7]]
+
+def sum_prod_diags(matrix, sum1 = 0, sum2 = 0)
+  (0..(matrix[0].length)-1).each do |column|
+    temp = 1
+    matrix.each_with_index do |sub_arr, index|
+      next unless sub_arr[index+column]
+
+      temp *= sub_arr[index+column]
+    end
+    sum1 += temp
+  end
+
+
+  sum1
+end
+
+puts sum_prod_diags MSSV
